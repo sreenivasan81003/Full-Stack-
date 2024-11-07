0446a47e50eae53846bad7c01419bc48a2c1a415
sreenivasan81003 <119567714+sreenivasan81003@users.noreply.github.com>
2024-11-07 13:37:59 +0530

    DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,14 +1,20 @@
+package com.yourpackage;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    public static Connection initializeDatabase() throws SQLException, ClassNotFoundException {
-        String jdbcURL = "jdbc:postgresql://localhost:5432/yourdatabase";
-        String username = "yourusername";
-        String password = "yourpassword";
-
-        Class.forName("org.postgresql.Driver");
-        return DriverManager.getConnection(jdbcURL, username, password);
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/your_db_name", 
+                "your_username", 
+                "your_password");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new SQLException("Connection failed", e);
+        }
     }
 }
+
