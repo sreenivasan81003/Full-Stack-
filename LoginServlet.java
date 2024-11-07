68a14f9307fcf7480dd1f27a1ce9a4a205069bbb
sreenivasan81003 <119567714+sreenivasan81003@users.noreply.github.com>
2024-11-07 13:24:05 +0530

    LoginServlet.java

---
+++ LoginServlet.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try (Connection conn = DatabaseConnection.initializeDatabase()) {
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                HttpSession session = request.getSession();
+                session.setAttribute("username", username);
+                session.setAttribute("role", rs.getString("role"));
+
+                switch (rs.getString("role")) {
+                    case "Employee":
+                        response.sendRedirect("requestAccess.jsp");
+                        break;
+                    case "Manager":
+                        response.sendRedirect("pendingRequests.jsp");
+                        break;
+                    case "Admin":
+                        response.sendRedirect("createSoftware.jsp");
+                        break;
+                }
+            } else {
+                response.sendRedirect("login.jsp");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
